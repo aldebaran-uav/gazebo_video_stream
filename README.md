9826ef9760db9ddb42512c330f868af53dc06e05
Hüseyin Karakaya <karakayahuseyin023@gmail.com>
2024-04-26 00:39:07 +0300

    gstream_video receiver

---
+++ README.md
@@ -0,0 +1,4 @@
+### ONEMLI !!! ###
+
+OpenCV source code ile build edilmeli.
+CMakeList dosyasında 297, 361 satırlarındaki seçenekler ON yapılmalı.
---
+++ gstream_video.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np 
+
+pipeline = 'udpsrc port=5600 caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264" ! rtph264depay ! avdec_h264 ! videoconvert ! appsink sync=false'
+cap = cv.VideoCapture(pipeline, cv.CAP_GSTREAMER)
+
+while True:
+
+    ret, frame = cap.read()
+
+
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+
+cap.release()
